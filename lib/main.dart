05d1b456ef36d518bad3614b100778b1a3d5a956
Sadid <sadid.jones@gmail.com>
2022-10-26 23:45:39 +0600

    @26/10/2022 @first example finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,20 +12,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: const HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+final currentDate = Provider<DateTime>(
+  (ref) => DateTime.now(),
+);
+
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hooks Riverpod'),
+      ),
+      body: Center(
+        child: Text(
+          date.toIso8601String(),
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
   }
 }
